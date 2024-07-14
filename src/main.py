fcc7de8d9b0bfd09dfa09678c833531748dde45a
Rob <robert.mclellan+github@gmail.com>
2024-07-14 14:22:52 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,41 +2,39 @@ import re
 import time
 import copy
 import configparser
-
-# My packages
-import parser
-import matcher
-from ynab_client import YNABClient
-from amazon_client.amazon_selenium_client import AmazonSeleniumClient
 from datetime import date, timedelta
+from amazon_client.amazon_selenium_client import AmazonSeleniumClient
+from actual_client import ActualClient
 
-# TODO: Use encrypted secrets config
 config = configparser.ConfigParser()
 config.read("secrets/credentials.ini")
 myConfig = config['DEFAULT']
+
 otpSecret = myConfig["otpSecret"]
 userEmail = myConfig["userEmail"]
 userPassword = myConfig["userPassword"]
-ynabToken = myConfig["ynabToken"]
+actualBaseUrl = myConfig["actualBaseUrl"]
+actualToken = myConfig["actualToken"]
 
 def main(amazonClient):
     orderIDs = amazonClient.getAllOrderIDs(3)
     amazonT = []
+
     for orderID in orderIDs:
         try:
             iPage = amazonClient.getInvoicePage(orderID)
             afterTaxItems, transactions = parser.parseInvoicePage(iPage)
-            if afterTaxItems == None or transactions == None:
+            if afterTaxItems is None or transactions is None:
                 continue
             matched = matcher.matchAmazonTransactions(afterTaxItems, transactions)
             amazonT.append(matched)
         except Exception as e:
             print(f"Something went wrong processing order {orderID}: {e}")
-    myYNAB = YNABClient(ynabToken)
-    ynabT = myYNAB.list_recent_amazon_transactions(date.today() - timedelta(days=30))
-    transactions = matcher.matchAmazonToYNAB(amazonT, ynabT)
-    myYNAB.patch_transactions(transactions)
 
+    actualClient = ActualClient(actualBaseUrl, actualToken)
+    actualT = actualClient.list_recent_transactions(30)
+    transactions = matcher.matchAmazonToYNAB(amazonT, actualT)
+    actualClient.update_transactions(transactions)
 
 if __name__ == "__main__":
     amazonSeleniumClient = AmazonSeleniumClient(userEmail, userPassword, otpSecret)
